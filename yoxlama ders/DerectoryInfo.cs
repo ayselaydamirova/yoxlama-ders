83bf8e18001400be11019d1e19bfe64269cde095
aysel <aydamirovaaysel7@gmail.com>
2021-11-13 21:00:28 +0400

    Добавьте файлы проекта.

---
+++ yoxlama ders/DerectoryInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace yoxlama_ders
+{
+    internal class DerectoryInfo
+    {
+        private string path;
+
+        public DerectoryInfo(string path)
+        {
+            this.path = path;
+        }
+
+        public bool Exists { get; internal set; }
+        public bool Name { get; internal set; }
+        public bool FullName { get; internal set; }
+        public bool CreatioanTime { get; internal set; }
+
+        internal void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool LastAccessTime { get; internal set; }
+        public bool LastWriteTime { get; internal set; }
+    }
+}
---
+++ yoxlama ders/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Threading;
+
+namespace yoxlama_ders
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+           Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-GB")
+
+            string path = @"C:\Users\Айсел\Documents";
+
+
+            DerectoryInfo dirInfo = new DerectoryInfo(path);
+
+            Console.WriteLine(dirInfo.Exists);
+            Console.WriteLine(dirInfo.Name);
+            Console.WriteLine(dirInfo.FullName);
+            Console.WriteLine(dirInfo.CreatioanTime);
+            Console.WriteLine(dirInfo.LastAccessTime);
+            Console.WriteLine(dirInfo.LastWriteTime);
+        }
+        
+    }
+}
